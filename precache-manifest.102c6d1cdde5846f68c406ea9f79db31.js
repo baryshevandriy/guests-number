c606d6ad152fffd553c1d4011d334f7b5aa552d5
baryshevandriy <baryshev.andriy@yandex.ru>
2023-01-12 00:22:12 +0300

    Updates

---
+++ precache-manifest.102c6d1cdde5846f68c406ea9f79db31.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "82dd57125c22c0c44a18",
+    "url": "/guests-number/static/css/main.d2f6b291.chunk.css"
+  },
+  {
+    "revision": "82dd57125c22c0c44a18",
+    "url": "/guests-number/static/js/main.82dd5712.chunk.js"
+  },
+  {
+    "revision": "02e73066ff27f2a42269",
+    "url": "/guests-number/static/js/1.02e73066.chunk.js"
+  },
+  {
+    "revision": "ae91e4ffb00e73bc09d9",
+    "url": "/guests-number/static/js/runtime~main.ae91e4ff.js"
+  },
+  {
+    "revision": "7c41c98aa74b3a7288b6d6da3ef90e87",
+    "url": "/guests-number/static/media/JetBrainsMono-Regular.7c41c98a.woff"
+  },
+  {
+    "revision": "0033a19bc96298c49c52e5c6522ae2ed",
+    "url": "/guests-number/static/media/JetBrainsMono-Regular.0033a19b.woff2"
+  },
+  {
+    "revision": "b20b68bac8b2b2c91a04b6131a045c00",
+    "url": "/guests-number/index.html"
+  }
+];
